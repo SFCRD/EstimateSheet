2708c91287c62d2ca81d2abaf21e18edab60b2aa
Scott M. Cullum <scott.cullum@akqa.com>
2011-06-23 11:18:50 -0700

    hello git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EstimateSheet::Application.config.secret_token = 'b7d79bca9d74aacbc4400ec346217f9b815fa4bf6e6b51b9a5d29e8c57a600a537ad2790a45fb3b5b0fc1ea458cc4554ea19e9a6f20fd96ef74d1feb1b02563d'
